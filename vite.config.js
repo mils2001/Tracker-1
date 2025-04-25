1042fa066de414821f86cc4db8099d991ea21341
Chrismiles <mileschris466@gmail.com>
2025-04-25 22:39:50 +0300

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // 👈 Make Vite output to "build" instead of "dist"
+  },
+});
+
